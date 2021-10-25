e117e4a8986bbb7f412bbbac671d0206d15d85a6
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-25 11:07:26 -0500

    [FIX] - Fix edge cases NULL

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,6 +10,8 @@ void shell_sort(int *array, size_t size)
 {
 	int i = 0, j = 0, k = 0, knuth = 1;
 
+	if (!array || !size || size < 2)
+		return;
 	while (knuth <= (int)(size - 1) / 3)
 		knuth = (3 * knuth) + 1;
 	while (knuth > 0)
