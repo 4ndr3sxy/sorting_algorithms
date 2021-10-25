86f676d0b48e8375429f186ad3d91326e938ce46
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-25 13:36:20 -0500

    [FIX] - Fix code, refactorized

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,32 @@
 #include "sort.h"
 
 /**
+ * search_index - search an new index in array
+ * @array: array with values
+ * @index: initial index
+ * @size: size of array
+ * Return: new index or -1
+ */
+int search_index(int *array, int size, int index)
+{
+	int min = 0, newIndex = 0, i = 0;
+
+	min = array[index];
+	newIndex = index;
+	for (i = index; i <= size - 1; i++)
+	{
+		if (array[i] < min)
+		{
+			min = array[i];
+			newIndex = i;
+		}
+	}
+	if (newIndex == index)
+		return (-1);
+	return (newIndex);
+}
+
+/**
  * selection_sort - algoritm that use the selection_sort
  * @array: array of integers
  * @size: size of array
@@ -8,31 +34,17 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0;
-	int indexValue = 0, valueSelection = 0, validateSwap = 0;
-	int temporalValue = 0;
+	int i = 0, j = 0, temporalValue = 0;
 
-	for (; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
-		validateSwap = 0;
-		valueSelection = array[i];
-		for (j = i; j < size; j++)
-		{
-			if (array[j] < valueSelection)
-			{
-				validateSwap = 1;
-				valueSelection = array[j];
-				indexValue = j;
-			}
-		}
-		if (validateSwap)
+		j = search_index(array, (int)size, i);
+		if (j >= 0)
 		{
 			temporalValue = array[i];
-			array[i] = valueSelection;
-			array[indexValue] = temporalValue;
+			array[i] = array[j];
+			array[j] = temporalValue;
 			print_array(array, size);
 		}
-		else
-			break;
 	}
 }
